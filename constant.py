089891ed6200014f6d62eafc38db39fdb5fbe16a
Aman Dalmia <amandalmia@Amans-MacBook-Pro.local>
2022-10-05 20:17:13 -0400

    added / modifed few more constants

--- constant.py
+++ constant.py
@@ -1,9 +1,12 @@
-
 import os
+
 game_type_map = {"regular_season": "02",
                  "playoffs": "03"}
 
-data_dir = os.path.dirname(os.path.abspath(__file__)) + os.path.sep + "data" + os.path.sep
+
+class Directory:
+    DATA_DIR = os.path.dirname(os.path.abspath(__file__)) + os.path.sep + "data" + os.path.sep
+
 
 class APIList():
     GET_ALL_MATCHES_FOR_A_GIVEN_SEASON = "https://statsapi.web.nhl.com/api/v1/schedule?season="
@@ -13,6 +16,8 @@ class APIList():
 class CustomRegex():
     REGULAR_GAME_ID = r"\d{0,4}02\d{0,4}"  # 02 for regular season
     PLAYOFFS_ID = r"\d{0,4}03\d{0,4}"  # 03 for playoffs
+
+TYPES_OF_SHOTS = ["Goal", "Shot"]
 
 # For playoff games, the 2nd digit of the specific number gives the round of the playoffs,
 # the 3rd digit specifies the matchup, and the 4th digit specifies the game (out of 7).
