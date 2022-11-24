0c26eb065ab05e1c58863db58b569509675b0fd6
jadevaibhav <vaibhavsjade@gmail.com>
2022-11-23 21:10:38 -0500

    latest xgb code

--- notebook/graphs.py
+++ notebook/graphs.py
@@ -11,12 +11,30 @@ from sklearn.metrics import classification_report
 from sklearn.metrics import plot_confusion_matrix
 from sklearn.datasets import make_classification
 from sklearn.metrics import roc_curve
-from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_auc_score,accuracy_score,f1_score
 from sklearn.calibration import CalibrationDisplay
 import warnings
 warnings.filterwarnings('ignore')
 
-def plot_cumulative_goal(comet_exp_obj, model, x_val, y_val):
+
+def log_comet(comet_exp_obj, name, model, x_val, y_val):
+    filename = '../model/' + name + "_Model.pkl"
+    pickle.dump(model, open(filename, 'wb'))
+    comet_exp_obj.log_model(name, file_or_folder=filename, overwrite=True, file_name=name + "_Model")
+
+    y_pred = model.predict(x_val)
+    y_prob = model.predict_proba(x_val)[:,1]
+    metrics = {
+        "Accuracy":accuracy_score(y_val,y_pred),
+        "Roc_auc":roc_auc_score(y_val,y_prob),
+        "f1_score":f1_score(y_val,y_pred),
+    }
+
+    comet_exp_obj.log_metrics(metrics)
+    comet_exp_obj.log_confusion_matrix(y_true=y_val, y_predicted=y_pred, title="Confusion Matrix", 
+                                           file_name="Confusion Matrix for "+name)
+
+def plot_cumulative_goal(comet_exp_obj,name, model, x_val, y_val):
     
     testy = y_val
     ns_probs = [0 for _ in range(len(y_val))]
@@ -37,7 +55,7 @@ def plot_cumulative_goal(comet_exp_obj, model, x_val, y_val):
 
 
     graph, (plt1) = plt.subplots(1, 1)
-    plt1.plot(goal_rate["percentile"], goal_rate["cum_perc"], label='Logistic Regression')
+    plt1.plot(goal_rate["percentile"], goal_rate["cum_perc"], label=name)
     plt1.set_ylim(0,100)
     plt1.invert_xaxis()
     plt1.set_xlabel('Shot probablity model percentile')
@@ -45,13 +63,13 @@ def plot_cumulative_goal(comet_exp_obj, model, x_val, y_val):
     plt.grid()
     plt.title("Cumulative % of goals")
     
-#     comet_exp_obj.log_figure(figure_name="Cumulative % of goals", figure=plt,
-#                    overwrite=False, step=None)
+    comet_exp_obj.log_figure(figure_name="Cumulative % of goals ", figure=plt,
+                    overwrite=False, step=None)
     
     plt.show()
 
 
-def create_extimator_plot(comet_exp_obj, model, x_val, y_val):
+def create_extimator_plot(comet_exp_obj,name, model, x_val, y_val):
     testy = y_val
     ns_probs = [0 for _ in range(len(y_val))]
     
@@ -59,15 +77,14 @@ def create_extimator_plot(comet_exp_obj, model, x_val, y_val):
     goal_prob = predicted_probablities[:, 1] # taking only the goal probablities
     
     disp = CalibrationDisplay.from_predictions(y_val, goal_prob)
-#     disp = CalibrationDisplay.from_estimator(model, x_val, y_val)
+    disp = CalibrationDisplay.from_estimator(model, x_val, y_val)
     
-    #comet_exp_obj.log_figure(figure_name="Calibration Display", figure=plt,
-    #             overwrite=False, step=None)
-    #comet_exp_obj.end()
+    comet_exp_obj.log_figure(figure_name="Calibration Display ", figure=plt,
+                 overwrite=False, step=None)
     
     plt.show()
 
-def plot_goal_shot_rate(comet_exp_obj, model, x_val, y_val):
+def plot_goal_shot_rate(comet_exp_obj,name, model, x_val, y_val):
     testy = y_val
     ns_probs = [0 for _ in range(len(y_val))]
     
@@ -84,7 +101,7 @@ def plot_goal_shot_rate(comet_exp_obj, model, x_val, y_val):
     goal_rate['percentile'] = goal_rate.index
 
     fig, (plt1) = plt.subplots(1, 1)
-    plt1.plot(goal_rate["percentile"], goal_rate["is_goal"], label='Logistic Regression')
+    plt1.plot(goal_rate["percentile"], goal_rate["is_goal"], label=name)
     plt1.set_ylim(0,100)
     plt1.xaxis.set_major_formatter('{x:1.0f}%')
     plt1.yaxis.set_major_formatter('{x:1.0f}%')
@@ -94,13 +111,13 @@ def plot_goal_shot_rate(comet_exp_obj, model, x_val, y_val):
     plt.grid()
     plt.title("Goal Rate")
 
-    #comet_exp_obj.log_figure(figure_name="Goal Rate", figure=plt,
-    #               overwrite=False, step=None)
+    comet_exp_obj.log_figure(figure_name="Goal Rate", figure=plt,
+                   overwrite=False, step=None)
     
     plt.show()
 
 
-def create_roc_auc_curve(comet_exp_obj, model, x_val, y_val):    
+def create_roc_auc_curve(comet_exp_obj,name, model, x_val, y_val):    
     testy = y_val
     ns_probs = [0 for _ in range(len(testy))]
 
@@ -119,7 +136,7 @@ def create_roc_auc_curve(comet_exp_obj, model, x_val, y_val):
     # plot the roc curve for the model
     plt.figure()
     plt.plot(ns_fpr, ns_tpr, linestyle='--', label='Base Line')
-    plt.plot(lr_fpr, lr_tpr, marker='.', label='Logistic')
+    plt.plot(lr_fpr, lr_tpr, marker='.', label=name)
     # axis labels
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
@@ -133,8 +150,8 @@ def create_roc_auc_curve(comet_exp_obj, model, x_val, y_val):
     #metrics = {"roc" : lr_auc}
     #comet_exp_obj.log_metrics(metrics)
     #comet_exp_obj.log_dataset_hash(x_train)
-    #comet_exp_obj.log_figure(figure_name="Receiver Operating Characteristic (ROC) curves and the AUC metric", 
-    #                         figure=plt, overwrite=False, step=None)
+    comet_exp_obj.log_figure(figure_name="Receiver Operating Characteristic (ROC) curves and the AUC metric", 
+                             figure=plt, overwrite=False, step=None)
     
     plt.show()
     
