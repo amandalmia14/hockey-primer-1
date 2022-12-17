372129db91f83c595da9fb0fa33ca7c37308af28
jadevaibhav <vaibhavsjade@gmail.com>
2022-12-16 19:30:19 -0500

    bug fix data last event data

--- docker-project-template-main/ift6758/ift6758/client/game_client.py
+++ docker-project-template-main/ift6758/ift6758/client/game_client.py
@@ -16,7 +16,7 @@ class GameClient:
         self.features = features
         self.data = None
         self.last_entry_idx = None
-        self.last_event_data = None
+       
         # any other potential initialization
 
     def get_live_data(self,game_id):
@@ -25,29 +25,33 @@ class GameClient:
         event_arr = game_dict['liveData']['plays']['allPlays']
         if len(event_arr) > self.last_entry_idx:
             
-            df = self.process_live_data(game_id,game_dict,self.last_entry_idx+1,self.last_event_data)
+            df = self.process_live_data(game_id,game_dict,self.last_entry_idx)
             self.last_entry_idx = len(event_arr) - 1 
-            self.last_event_data = last_data_parsing(event_arr[-1])
+            
 
         return df
 
-    def process_live_data(self,game_id,game_dict,entry_idx,last_event_data):
+    def process_live_data(self,game_id,game_dict,entry_idx):
         period_dict = get_side(game_meta=game_dict)
         teams_type = get_home_away_team(game_meta=game_dict)
         live_data = game_dict["liveData"]["plays"]["allPlays"]
         final_list = []
-        for i in live_data[entry_idx:]:
+        last_event = live_data[entry_idx]
+        for i in live_data[entry_idx+1:]:
             if i["result"]["event"] in TYPES_OF_SHOTS:
-                try:
-                    parsed_data = data_parsing(data=i, id=game_id, event_type=i["result"]["event"],
-                                            period_dict=period_dict, team_detail_dict=teams_type,
-                                            last_event_data=last_event_data)
-                    final_list.append(parsed_data)
-                except Exception as e:
-                    print(e)
-                    import traceback
-                    print(traceback.print_exc())
-                    break
+                    try:
+                        last_event_data = last_data_parsing(last_event)
+                        parsed_data = data_parsing(data=i, id=game_id, event_type=i["result"]["event"],
+                                                   period_dict=period_dict, team_detail_dict=teams_type,
+                                                   last_event_data=last_event)
+                        final_list.append(parsed_data)
+                    except Exception as e:
+                        print(game_id)
+                        print(e)
+                        import traceback
+                        print(traceback.print_exc())
+                        break
+            last_event = i
 
         shots_goals_df = pd.DataFrame(final_list)
         return shots_goals_df
