f74a9b91db02c97b1dc2cb67eaf95becf5767fce
jadevaibhav <vaibhavsjade@gmail.com>
2022-12-19 14:36:27 -0500

    empty data frame

--- docker-project-template-main/ift6758/ift6758/client/game_client.py
+++ docker-project-template-main/ift6758/ift6758/client/game_client.py
@@ -9,6 +9,7 @@ class GameClient:
     def __init__(self, ip: str = "0.0.0.0", port: int = 5001, features=None):
         self.base_url = f"http://{ip}:{port}"
         logger.info(f"Initializing client; base URL: {self.base_url}")
+
         if features is None:
             features = ["distance"]
         self.features = features
@@ -21,11 +22,21 @@ class GameClient:
         # diff based on all event types, but our df only concerns about shot and goal events
         game_dict = get_data_by_gameid(game_id)
         event_arr = game_dict['liveData']['plays']['allPlays']
-        print("self.last_entry_idx", self.last_entry_idx)
         if len(event_arr) > self.last_entry_idx:
             df = self.process_live_data(game_id, game_dict, self.last_entry_idx)
             self.last_entry_idx = len(event_arr) - 1
-        
+        else:
+            df = pd.DataFrame(columns=['game_id', 'event_code', 'player_info', 'shooter', 'goalie', 'event',
+            'event_type_id', 'event_description', 'home_team', 'home_team_abv',
+            'away_team', 'away_team_abv', 'about_event_id', 'about_period',
+            'about_period_type', 'game_time', 'about_time_remaining',
+            'about_date_time', 'about_goal_away', 'about_goal_home',
+            'action_team_name', 'last_event_code', 'last_event_type_id',
+            'last_event_coordinates', 'last_event_time', 'last_event_period',
+            'event_secondary_type', 'coordinates', 'home_team_side',
+            'away_team_side', 'event_strength_name', 'event_strength_code',
+            'event_game_winning_goal', 'event_empty_net'])
+            
         return df
 
     def process_live_data(self, game_id, game_dict, entry_idx):
