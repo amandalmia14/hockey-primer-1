b13be1f81532a83ca58ca8101134c9cafcba7045
Aman Dalmia <amandalmia@Amans-MacBook-Pro.local>
2022-12-20 21:50:50 -0500

    bug fix for invalid game id

--- docker-project-template-main/ift6758/ift6758/client/game_client.py
+++ docker-project-template-main/ift6758/ift6758/client/game_client.py
@@ -21,23 +21,29 @@ class GameClient:
     def get_live_data(self, game_id):
         # diff based on all event types, but our df only concerns about shot and goal events
         game_dict = get_data_by_gameid(game_id)
-        event_arr = game_dict['liveData']['plays']['allPlays']
-        if len(event_arr) > self.last_entry_idx:
-            df = self.process_live_data(game_id, game_dict, self.last_entry_idx)
-            self.last_entry_idx = len(event_arr) - 1
+        message = "Success"
+        empty_df = pd.DataFrame(columns=['game_id', 'event_code', 'player_info', 'shooter', 'goalie', 'event',
+                'event_type_id', 'event_description', 'home_team', 'home_team_abv',
+                'away_team', 'away_team_abv', 'about_event_id', 'about_period',
+                'about_period_type', 'game_time', 'about_time_remaining',
+                'about_date_time', 'about_goal_away', 'about_goal_home',
+                'action_team_name', 'last_event_code', 'last_event_type_id',
+                'last_event_coordinates', 'last_event_time', 'last_event_period',
+                'event_secondary_type', 'coordinates', 'home_team_side',
+                'away_team_side', 'event_strength_name', 'event_strength_code',
+                'event_game_winning_goal', 'event_empty_net'])
+        if "message" in game_dict:
+            message = "Game data couldn't be found / Invalid Game Id"
+            df = empty_df
         else:
-            df = pd.DataFrame(columns=['game_id', 'event_code', 'player_info', 'shooter', 'goalie', 'event',
-            'event_type_id', 'event_description', 'home_team', 'home_team_abv',
-            'away_team', 'away_team_abv', 'about_event_id', 'about_period',
-            'about_period_type', 'game_time', 'about_time_remaining',
-            'about_date_time', 'about_goal_away', 'about_goal_home',
-            'action_team_name', 'last_event_code', 'last_event_type_id',
-            'last_event_coordinates', 'last_event_time', 'last_event_period',
-            'event_secondary_type', 'coordinates', 'home_team_side',
-            'away_team_side', 'event_strength_name', 'event_strength_code',
-            'event_game_winning_goal', 'event_empty_net'])
+            event_arr = game_dict['liveData']['plays']['allPlays']
+            if len(event_arr) > self.last_entry_idx:
+                df = self.process_live_data(game_id, game_dict, self.last_entry_idx)
+                self.last_entry_idx = len(event_arr) - 1
+            else:
+                df = empty_df
             
-        return df
+        return df, message
 
     def process_live_data(self, game_id, game_dict, entry_idx):
         period_dict = get_side(game_meta=game_dict)
